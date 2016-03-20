b61fba87f5de49e004611d5c18ea17fb62926b05
ccy <ccy0524@163.com>
2016-03-20 17:08:42 +0800

    three

--- weekend110/src/cn/itcast/hadoop/mr/habase/HabaseMR.java
+++ weekend110/src/cn/itcast/hadoop/mr/habase/HabaseMR.java
@@ -16,36 +16,34 @@ import org.apache.hadoop.mapred.Reducer;
 import org.apache.hadoop.mapreduce.Job;
 
 public class HabaseMR {
-	public static void main(String[] args) throws IOException{
-		Configuration conf=HBaseConfiguration.create();
-		Job job=new Job(conf,"maperduce on habase");
+	public static void main(String[] args) throws IOException {
+		Configuration conf = HBaseConfiguration.create();
+		Job job = new Job(conf, "maperduce on habase");
 		job.setJarByClass(HabaseMR.class);
-				
+
 	}
 
-	public class Mapper extends TableMapper<Text, Text>{
+	public class Mapper extends TableMapper<Text, Text> {
 		@Override
-		protected void map(ImmutableBytesWritable key, Result value,
-				Context context)
+		protected void map(ImmutableBytesWritable key, Result value, Context context)
 				throws IOException, InterruptedException {
-			
-			Text k=new Text(Bytes.toString(key.get()));
-			Text v=new Text(Bytes.toString(value.getValue(Bytes.toBytes("basicInfo"), Bytes.toBytes("age"))));
-			
+
+			Text k = new Text(Bytes.toString(key.get()));
+			Text v = new Text(Bytes.toString(value.getValue(Bytes.toBytes("basicInfo"), Bytes.toBytes("age"))));
+
 			context.write(v, k);
 		}
 	}
-	
-	public class Reducer extends TableReducer<Text, Text, ImmutableBytesWritable>{
+
+	public class Reducer extends TableReducer<Text, Text, ImmutableBytesWritable> {
 		@Override
-		protected void reduce(Text key, Iterable<Text> values,
-				Context context)
+		protected void reduce(Text key, Iterable<Text> values, Context context)
 				throws IOException, InterruptedException {
-			Put put=new Put(Bytes.toBytes(key.toString()));
-			for(Text value:values){
-				put.add(Bytes.toBytes("f1"),Bytes.toBytes(value.toString()),Bytes.toBytes(value.toString()));
+			Put put = new Put(Bytes.toBytes(key.toString()));
+			for (Text value : values) {
+				put.add(Bytes.toBytes("f1"), Bytes.toBytes(value.toString()), Bytes.toBytes(value.toString()));
 			}
-			
+
 			context.write(null, put);
 		}
 	}
